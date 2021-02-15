01ebacd89006d0b0209222aab968be81baae5474
Preeti Yuankrathok <preetisatit@gmail.com>
2021-02-15 11:14:57 +0700

    Fix exception not too wide

--- flipt/clients.py
+++ flipt/clients.py
@@ -6,5 +6,5 @@ from django.conf import settings
 try:
     channel = grpc.insecure_channel(settings.FLIPT_GRPC_HOST)
     client = FliptStub(channel=channel)
-except Exception:
+except (AttributeError, grpc.RpcError):
     client = None
