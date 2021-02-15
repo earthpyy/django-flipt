8efd496aedc530b9381c300b8dc8f018da730882
Preeti Yuankrathok <preetisatit@gmail.com>
2021-02-15 11:04:12 +0700

    Set client to None when cannot connect

--- flipt/clients.py
+++ flipt/clients.py
@@ -3,6 +3,8 @@ import grpc
 from flipt_pb2_grpc import FliptStub
 from django.conf import settings
 
-
-channel = grpc.insecure_channel(settings.FLIPT_GRPC_HOST)
-client = FliptStub(channel=channel)
+try:
+    channel = grpc.insecure_channel(settings.FLIPT_GRPC_HOST)
+    client = FliptStub(channel=channel)
+except Exception:
+    client = None
